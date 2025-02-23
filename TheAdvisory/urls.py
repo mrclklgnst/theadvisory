512517d2a0299392b7375e594fce808a7fb7e527
mrclklgnst <mkilgenstein@gmail.com>
2025-02-23 10:11:35 +0100

    added PA urls to TOA urls

--- TheAdvisory/urls.py
+++ TheAdvisory/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("PoliticalAdvisor/", include("PoliticalAdvisor.urls")),
+    path("", include("PoliticalAdvisor.urls")),
 ]
